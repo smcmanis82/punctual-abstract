9daf455aa3d55225083c9126750e719b77bb19b1
smcmanis82 <sharon.mcmanis@gmail.com>
2021-11-10 12:48:11 -0600

    added tests

--- rotate.js
+++ rotate.js
@@ -1,6 +1,10 @@
 function rotateArray(numList) {
-  let num = numList.shift();
-  numList.push(num);
+  let loopLimit = numList[0];
+  for (var i = 0; i < loopLimit; i++) {
+    let num = numList.shift();
+    numList.push(num);
+  }
+
   return numList.join('');
 }
 
--- test/rotate.test.js
+++ test/rotate.test.js
@@ -2,12 +2,28 @@ const assert = require('assert');
 const rotateArray = require('../rotate').rotateArray;
 
 describe('rotateArray()', function () {
-  it('should return - an array shifted, pushed and joined', function () {
-    let input = [3, 2, 9, 4, 5, 1];
+  it('should return - empty array test', function () {
+    let input = [];
     let actual = rotateArray(input);
-    console.log(actual, '11');
-    let expected = '294513';
-    console.log(expected, '99');
+    let expected = '';
+    assert.equal(actual, expected);
+  });
+  it('should return - small array test - rotate one time', function () {
+    let input = [1, 2, 3];
+    let actual = rotateArray(input);
+    let expected = '231';
+    assert.equal(actual, expected);
+  });
+  it('should return - small array test - rotate twice', function () {
+    let input = [2, 1, 3];
+    let actual = rotateArray(input);
+    let expected = '321';
+    assert.equal(actual, expected);
+  });
+  it('should return - small array test - rotate three times', function () {
+    let input = [3, 1, 2, 4];
+    let actual = rotateArray(input);
+    let expected = '4312';
     assert.equal(actual, expected);
   });
 });
